711ba30cfc4f4b2d421e95ba014e866da2fc0d3a
= <=>
2025-12-22 22:53:12 -0300

    rotas de login e register usuario controller

---
+++ src/main/java/com/projeto_estudo_spring/controller/AuthController.java
@@ -0,0 +1,48 @@
+package com.projeto_estudo_spring.controller;
+
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projeto_estudo_spring.model.Usuario;
+import com.projeto_estudo_spring.security.JwtUtil;
+import com.projeto_estudo_spring.service.UsuarioService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody Map<String, String> request) {
+        Usuario usuario = usuarioService.registrarUsuario(request.get("username"),"password");
+        return ResponseEntity.ok(usuario);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Map<String, String> request) {
+        Optional<Usuario> usuario = usuarioService.buscarPorUsername(request.get("username"));
+
+        if(usuario.isPresent() && usuario.get().getPassword().equals(request.get("password"))){
+            String token = JwtUtil.generateToken(usuario.get().getUsername());
+            return ResponseEntity.ok(Map.of("token", token));
+        }
+        return ResponseEntity.status(401).body("Credenciais invalidas");
+            
+    }
+    
+    
+
+
+
+
+}
